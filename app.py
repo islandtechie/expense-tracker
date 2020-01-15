aca74dcd6ceb095e01284decef5e3b80d1451f23
Berlin Smith <berlin.smithjr@gmail.com>
2020-01-15 04:26:03 -0500

    working on backend

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 import datetime
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, make_response, url_for, redirect
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='../client/build/static', template_folder='../client/build')
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{}:{}@{}/{}".format(os.environ.get('PGUSER'), os.environ.get('PGPASSWORD'), os.environ.get('PGHOST'), os.environ.get('PGDATABASE'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 api = Api(app)
 
 class Users(db.Model):
@@ -17,13 +20,21 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(255), nullable=False)
     lastname = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     registered = db.Column(db.DateTime, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    modified = db.Column(db.DateTime, nullable=False)
+
+class UsersSchema(ma.ModelSchema):
+    class Meta:
+        model = Users
 
 class Expenses(db.Model):
     __tablename__ = 'expenses'
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     expense_date = db.Column(db.DateTime, nullable=False)
     description = db.Column(db.String(255), nullable=False)
     purchase_place = db.Column(db.String(255), nullable=False)
@@ -35,7 +46,7 @@ class Auth(db.Model):
     __tablename__ = 'auth'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     created = db.Column(db.DateTime, nullable=False)
     modified = db.Column(db.DateTime, nullable=False)
@@ -45,37 +56,64 @@ class Auth(db.Model):
 
 
 class User(Resource):
-    def get(self):
-        return 'some'
+    def get(self, user_id):
+        user = Users.query.filter_by(id=user_id).first()
+        users_schema = UsersSchema()
+        return users_schema.dump(user)
     
     def post(self):
         return 'gogo'
 
 class Auth(Resource):
+    def get(self):
+        return 'login form'
     def post(self):
         return 'auth'
 
-class Register(Resource):
+
+        
+
+
+class Expenses(Resource):
     def post(self):
-        data = Users(
-            firstname=request.form['firstname'],
-            lastname=request.form['lastname'],
-            email=request.form['email'],
-            registered=datetime.datetime.now()
-        )
-        print(request.form['firstname'])
-        db.session.add(data)
-        db.session.commit()
-        return 'register'
+        return 'expenses'
 
-api.add_resource(User, '/user/:id')
+
+def loginUser(username, password):
+    print(username)
+    print(password)
+    user = Users.query.filter_by(email=username).first()
+    if (check_password_hash(user.password, password)):
+        return 'passed login'
+    else:
+        return 'failed please check username and/or password'
+
+api.add_resource(User, '/user/<user_id>')
+api.add_resource(Expenses, '/expenses/<id>')
 api.add_resource(Auth, '/auth')
-api.add_resource(Register, '/register')
 
+@app.route('/register', methods=['POST'])
+def register():
+    data = Users(
+        firstname=request.form['firstname'],
+        lastname=request.form['lastname'],
+        email=request.form['email'],
+        registered=datetime.datetime.now(),
+        created=datetime.datetime.now(),
+        modified=datetime.datetime.now(),
+        password=generate_password_hash(request.form['password'])
+    )
+    db.session.add(data)
+    db.session.commit()
+    db.session.refresh(data)
+    if (data.id):
+        return jsonify({"success": True}), 201
+    else:
+        return jsonify({"success": False})
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-    return 'login'
+    return loginUser(request.form['username'], request.form['password'])
 
 @app.route('/logout')
 def logout():
--- db.py
+++ db.py
@@ -1,16 +1,14 @@
 from dotenv import load_dotenv
-import sys
 import os
-
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import  declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+import sys
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base 
 
 load_dotenv()
 connection_string = "postgresql://{}:{}@{}/{}".format(os.environ.get('PGUSER'), os.environ.get('PGPASSWORD'), os.environ.get('PGHOST'), os.environ.get('PGDATABASE'))
 engine = create_engine(connection_string)
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine, autoflush=True)
 session = Session()
 Base = declarative_base()
 
@@ -19,9 +17,12 @@ class Users(Base):
 
     id = Column(Integer, primary_key=True)
     firstname = Column(String(255), nullable=False)
-    lasttname = Column(String(255), nullable=False)
+    lastname = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False)
     registered = Column(DateTime, nullable=False)
+    created = Column(DateTime, nullable=False)
+    modified = Column(DateTime, nullable=False)
 
 class Expenses(Base):
     __tablename__ = 'expenses'
@@ -35,7 +36,7 @@ class Expenses(Base):
     modified = Column(DateTime, nullable=False)
 
 class Auth(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'auth'
 
     id = Column(Integer, primary_key=True)
     firstname = Column(String(255), nullable=False)
