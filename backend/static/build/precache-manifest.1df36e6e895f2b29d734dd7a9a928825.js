d445a4611a0670417f1a942e7ddfa1f3769e44c5
Berlin Smith <berlin.smithjr@gmail.com>
2020-01-23 20:07:51 -0500

    re-added react app

---
+++ backend/static/build/precache-manifest.1df36e6e895f2b29d734dd7a9a928825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef7ddd274121f7e5d5fc9f2a9bdaa4d",
+    "url": "/static/build/../../../../template/index.html"
+  },
+  {
+    "revision": "1020b7e08a90d0e4ff70",
+    "url": "/static/build/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163dd15cca523a63d899",
+    "url": "/static/build/js/2.970da5d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/build/js/2.970da5d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1020b7e08a90d0e4ff70",
+    "url": "/static/build/js/main.02d7268c.chunk.js"
+  },
+  {
+    "revision": "7477b8bc6a47049e76f1",
+    "url": "/static/build/js/runtime-main.ecb1abd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/build/media/logo.5d5d9eef.svg"
+  }
+]);
