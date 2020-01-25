25fe8411e400cddf3ee213dfe1d11c0140f4bd58
Berlin Smith <berlin.smithjr@gmail.com>
2020-01-24 23:32:01 -0500

    working on api

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 
-app = Flask("__main__")
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+    def get(self):
+        return "User GET"
+
+    def post(self):
+        return {'about' : "user post"}
+
+class Expense(Resource):
+    def get(self):
+        return "Expense Get"
+
+    def post(self):
+        return {"Expense Post": "nada"}
+
+api.add_resource(User, '/user')
+api.add_resource(Expense, '/expense')
 
 @app.route("/")
 def index():
-    return 'Hello world'
-
+    return render_template("index.html")
