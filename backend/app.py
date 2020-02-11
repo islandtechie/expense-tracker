22e4a203c5b34608c08df2d5ef87cb0268e9a15a
Berlin Smith <berlin.smithjr@gmail.com>
2020-02-10 23:08:53 -0500

    working on login

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 import os, datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt import JWT, jwt_required, current_identity
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 
@@ -9,16 +11,26 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 api = Api(app)
 
+app.config['SECRET_KEY'] = 'secret-key'
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     registered_date = db.Column(db.DateTime, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     modified_at = db.Column(db.DateTime, nullable=False)
     #expenses = db.relationship('Expenses', backref="payer", lazy=True)
     #auth = db.relationship('Expenses', backref="auth", lazy=True)
+
+class Auth(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    session = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    modified_at = db.Column(db.DateTime, nullable=False)
 
 class Register(Resource):
     def post(self):
@@ -28,6 +40,7 @@ class Register(Resource):
                 first_name = request.form['firstname'],
                 last_name = request.form['lastname'],
                 email = request.form['email'],
+                password = generate_password_hash(request.form['password'],method='pbkdf2:sha256', salt_length=10),
                 registered_date = datetime.datetime.utcnow(),
                 created_at = datetime.datetime.utcnow(),
                 modified_at = datetime.datetime.utcnow()
@@ -44,6 +57,32 @@ class Register(Resource):
                         raise
 
         else:
-            return {'error' : 'Please enter your credentials and try again.'}
-            
+            return {'error' : 'Please enter your the information entered and try again.'}
+
+class Login(Resource):
+    def post(self):
+        if (request.form):
+            user = User.query.filter_by(email=request.form['email']).first()
+            if (user and check_password_hash(user.password, request.form['password'])):
+                auth =  Auth(
+                    user_id = user.id,
+                    session = abs(hash(str(user.id) + str(datetime.datetime.now()))),
+                    created_at = datetime.datetime.now(),
+                    modified_at = datetime.datetime.now()
+                )
+                db.session.add(auth)
+                db.session.commit()
+                return jsonify(
+                    id=user.id,
+                    firstname=user.first_name,
+                    lastname=user.last_name,
+                    session=auth.session
+                )
+            else:
+                return 'Please check your credentials or register for an account'
+
+        else:
+            return {'error' : 'Please enter your credentials'}
+
 api.add_resource(Register, '/register')
+api.add_resource(Login, '/login')
